c9b5ad0fe29226a1c922cc95d9527e9151abef8d
collin-mccarter <collinmccarter12@gmail.com>
2023-03-04 20:24:57 -0600

    verbage changes

--- Assets/script.js
+++ Assets/script.js
@@ -9,7 +9,7 @@ var initialsEl = document.querySelector("#initials");
 
 
 var currentQuestionIndex = 0;
-var time = questions.length * 15;
+var time = questions.length * 10;
 var timerId;
 
 startBtn.onclick = startQuiz;
