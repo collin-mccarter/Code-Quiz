9717b10e9088bd2ff2c6d28c99b1f3c2cf65c078
collin-mccarter <collinmccarter12@gmail.com>
2023-03-04 21:00:26 -0600

    Added more questions

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,7 +1,52 @@
 let questions = [
     {
-        title: "Title test",
-        choices: ["A", "B", "C"],
-        answer: "A"
+        title: "Javascript is an _______ language?",
+        choices: ["Object Oriented", "Object Based", "Procedural", "None of the Above"],
+        answer: "Object Oriented"
+    },
+    {
+        title: "Of the following, if statements, which one correctly executes three instructions if the condition is true?",
+        choices: ["A. If (x < 0) a = b * 2; y = x; z = a – y;", "B. { if (x < 0) a = b * 2; y = x; z = a – y; }", "C. If{ (x < 0) a = b * 2; y = x; z = a – y ; }", "D. If (x < 0) { a = b * 2; y = x; z = a – y; }"],
+        answer: "D. If (x < 0) { a = b * 2; y = x; z = a – y; }"
+    },
+    {
+        title: "Which of the following keywords is used to define a variable in Javascript?",
+        choices: ["var", "let", "Both var and let", "None of the above"],
+        answer: "Both var and let"
+    },
+    {
+        title: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: ["getElementbyId()","getElementsByClassName()","Both A and B","Neither A or B"],
+        answer: "Both A and B"
+    },
+    {
+        title: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        choices: ["Throws an error", "Ignores the statements","Gives a warning","Throws up"],
+        answer: "Ignores the statements"
+    },
+    {
+        title: "Which of the following methods can be used to display data in some form using Javascript?",
+        choices: ["document.write()", "console.log()","window.alert()","All of the above"],
+        answer: "All of the above"
+    },
+    {
+        title:"How can a datatype be declared to be a constant type?",
+        choices: ["const","var","let","constant"],
+        answer: "const"
+    },
+    {
+        title:"What does the Javascript “debugger” statement do?",
+        choices: ["It will debug all the errors in the program at runtime","It acts as a breakpoint in a program","It will debug error in the current statement if any","All of the above"],
+        answer:"It acts as a breakpoint in a program"
+    },
+    {
+        title:"How do we write a comment in javascript?",
+        choices: ["/* */", "//","#","<--- ! --->"],
+        answer:"//"
+    },
+    {
+        title: "Which one of the following also known as Conditional Expression:",
+        choices: ["Alternative to if-else","Switch statement","If-then-else statement","immediate if"],
+        answer: "immediate if"
     }
 ];
