e7d253bbda6831916e84667974b3248aab2743f6
collin-mccarter <collinmccarter12@gmail.com>
2023-03-05 18:23:45 -0600

    added score comments

--- Assets/score.js
+++ Assets/score.js
@@ -1,24 +1,31 @@
+// function to print highscores
 function printHighscores() {
-  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  // gets stored highscores from local storage
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || []; 
   
+  // sorts scores from highest to lowest value
   highscores.sort(function(a, b) {
     return b.score - a.score;
   });
   
+  // function to create each highscore as a list element
   highscores.forEach(function(score) {
-    var liTag = document.createElement("li");
-    liTag.textContent = score.initials + " - " + score.score;
+    // creates li
+    var liTag = document.createElement("li"); 
+    liTag.textContent = score.initials + " - " + score.score; // adds initials to li
 
     var olEl = document.getElementById("highscores");
-    olEl.appendChild(liTag);
+    olEl.appendChild(liTag); // creates highscore and adds it to list
   });
 }
   
+// removes all stores highscore data from local storage
 function clearHighscores() {
   window.localStorage.removeItem("highscores");
   window.location.reload();
 }
   
-document.getElementById("clear").onclick = clearHighscores;
-  
-printHighscores();
+document.getElementById("clear").onclick = clearHighscores; // linking button on click to execute clearHighscores function
+
+// prints out the highscores at the end of the function
+printHighscores(); 
